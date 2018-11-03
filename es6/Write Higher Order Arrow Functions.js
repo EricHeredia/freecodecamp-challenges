32bd77baa0544f6f21632fd15a6d39ae106bb8ea
EricHeredia <EricDavidHeredia@gmail.com>
2018-11-02 23:10:10 -0500

    Fixing file

--- es6/Write Higher Order Arrow Functions.js
+++ es6/Write Higher Order Arrow Functions.js
@@ -2,11 +2,10 @@ const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
 const squareList = (arr) => {
   "use strict";
   // change code below this line
-  const squaredIntegers = arr.filter(i => Number.isInteger(i) && i > 0).map(i => i * i);
+  const squaredIntegers = arr.filter(i => Number.isInteger(i) && i > 0).map(i => Math.pow(i, 2));
   // change code above this line
   return squaredIntegers;
 };
-console.log(2.2 % parseInt("2.2"));
 // test your code
 const squaredIntegers = squareList(realNumberArray);
 console.log(squaredIntegers);
